e6ac37915d788ff0d58aadcbab01aa1c97278a30
Mon Noval <msnoval@d-edge.com>
2021-07-14 10:28:02 +0800

    t440s initial config

---
+++ t440s-default.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import re
+from xkeysnail.transform import *
+
+# [Global modemap] Change modifier keys as in xmodmap
+define_modmap({
+
+    # Thinkpad T440s default keyboard
+    Key.LEFT_ALT: Key.LEFT_CTRL,
+    Key.LEFT_CTRL: Key.LEFT_ALT,
+    Key.CAPSLOCK: Key.BACKSPACE,
+
+    # Thinkpad T440s trackpad buttons
+    Key.BTN_MIDDLE: Key.ESC,
+})
+
+# Keybindings for Browsers
+define_keymap(re.compile("Firefox|firefox|Chromium|LibreWolf"), {
+
+    # Next and previous tab
+    K("C-left"): K("C-Shift-TAB"),
+    K("C-right"): K("C-TAB"),
+    K("LC-LShift-H"): K("C-Shift-TAB"),
+    K("LC-LShift-L"): K("C-TAB"),
+
+    # Browser back and forward
+    K("LSuper-H"): K("LM-LEFT"),
+    K("LSuper-L"): K("LM-RIGHT"),
+
+}, "Browsers")
+
+# Affects all apps
+define_keymap(lambda wm_class: wm_class not in ("AnAppThatDoesNotExist"), {
+
+    # Thinkpad T440s default keyboard
+    K("RShift-T"): [launch(["xfce4-terminal"])],
+    K("RShift-V"): [launch(["nvim-qt"])],
+    K("RShift-X"): [launch(["xkill"])],
+    K("RShift-F"): [launch(["thunar"])],
+    K("RShift-W"): [launch(["firefox","-new-tab","about:newtab"])],
+
+    # Thinkpad T440s trackpad buttons
+    K("BTN_RIGHT"): K("ENTER"),
+    K("LC-BTN_RIGHT"): K("BTN_RIGHT"),
+
+}, "All apps")
