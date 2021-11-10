839ea3ccdfeef64cdc21a1f1b870af4753c3c876
Mon Noval <msnoval@d-edge.com>
2021-11-10 09:10:20 +0800

    acernitro

---
+++ acernitro-kinesis-elecomhuge.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import re
+from xkeysnail.transform import *
+
+# [Global modemap] Change modifier keys as in xmodmap
+define_modmap({
+
+    # Kinesis Advantage
+    Key.RIGHT_META: Key.RIGHT_CTRL,
+    Key.LEFT_CTRL: Key.RIGHT_META,
+
+    # ELECOM HUGE
+    Key.BTN_BACK: Key.ENTER,
+    Key.BTN_SIDE: Key.ENTER,
+    Key.BTN_EXTRA: Key.BTN_MOUSE,
+
+})
+
+# [Conditional modmap] Change modifier keys in certain applications
+define_conditional_modmap(re.compile('nvim-qt'), {
+    # Kinesis Advantage
+    Key.RIGHT_META: Key.RIGHT_CTRL,
+})
+
+# Keybindings for Browsers
+define_keymap(re.compile("nvim-qt"), {
+    # ELECOM HUGE
+    K("BTN_FORWARD"): K("C-q"),
+}, "Editor")
+
+# Keybindings for Browsers
+define_keymap(re.compile("Firefox|firefox|Google-chrome|Chromium|LibreWolf"), {
+    K("C-right"): K("C-TAB"),
+    K("C-left"): K("C-Shift-TAB"),
+}, "Browsers")
+
+# Affects all apps
+define_keymap(lambda wm_class: wm_class not in ("AnAppThatDoesNotExist"), {
+
+    # Gnome for Fedora Workstation
+    K("RShift-T"): [launch(["gnome-terminal"])],
+    K("RShift-V"): [launch(["nvim-qt"])],
+    # K("RShift-X"): [launch(["xkill"])],
+    K("RShift-F"): [launch(["nautilus"])],
+    K("RShift-W"): [launch(["firefox","-new-tab","about:newtab"])],
+
+    # ELECOM HUGE
+    K("BTN_FORWARD"): K("C-w"),
+    K("BTN_TASK"): K("C-Space"),
+
+    # Kinesis Advantage
+    K("HOME"): K("C-Space"),
+
+}, "All apps")
---
+++ run-acernitro-work.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xkeysnail .xkeysnail-config/acernitro-kinesis-elecomhuge.py --devices /dev/input/event6 /dev/input/event9 --quiet </dev/null &>/dev/null &
