cef71b93ea6dbef02f4c709d04748b7c77e1ebc3
Mon Noval <msnoval@d-edge.com>
2022-04-01 10:19:03 +0800

    absolute home dir

--- run-pc.sh
+++ run-pc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-xkeysnail .xkeysnail-config/pc-kinesis-elecomexg.py --devices /dev/input/event3 /dev/input/event6 --quiet </dev/null &>/dev/null &
-# xkeysnail .xkeysnail-config/pc-kinesis-elecomexg.py --devices /dev/input/event3 /dev/input/event8 --quiet </dev/null &>/dev/null &
-# xkeysnail .xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event9 --quiet </dev/null &>/dev/null &
+xkeysnail $HOME/.xkeysnail-config/pc-kinesis-elecomexg.py --devices /dev/input/event3 /dev/input/event6 --quiet </dev/null &>/dev/null &
+# xkeysnail $HOME/.xkeysnail-config/pc-kinesis-elecomexg.py --devices /dev/input/event3 /dev/input/event8 --quiet </dev/null &>/dev/null &
+
+# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event9 --quiet </dev/null &>/dev/null &
--- run-t430s.sh
+++ run-t430s.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xkeysnail .xkeysnail-config/t430s-kinesis-elecomhuge.py --devices /dev/input/event4 /dev/input/event6 --quiet </dev/null &>/dev/null &
+xkeysnail $HOME/.xkeysnail-config/t430s-kinesis-elecomhuge.py --devices /dev/input/event4 /dev/input/event6 --quiet </dev/null &>/dev/null &
