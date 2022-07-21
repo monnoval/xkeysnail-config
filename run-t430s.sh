e99292c3ce16ecf8de171983a2e85430075d31bf
Mon Noval <msnoval@d-edge.com>
2022-07-21 13:50:12 +0800

    better t430s

--- run-t430s.sh
+++ run-t430s.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-xkeysnail $HOME/.xkeysnail-config/t430s-kinesis-elecomhuge.py --devices /dev/input/event4 /dev/input/event6 --quiet </dev/null &>/dev/null &
+# xkeysnail ~/.xkeysnail-config/t430s-kinesis-elecomhuge.py --devices /dev/input/event8 /dev/input/event12 --quiet </dev/null &>/dev/null &
+
+# With mouse+keyboard
+# xkeysnail ~/.xkeysnail-config/t430s-kinesis-elecomhuge.py --devices /dev/input/event8 /dev/input/event11 --quiet </dev/null &>/dev/null &
+# xkeysnail ~/.xkeysnail-config/t430s-kinesis-elecomhuge.py --devices /dev/input/event8 /dev/input/event12 --quiet </dev/null &>/dev/null &
+
+# Laptop only without mouse+keyboard
+xkeysnail ~/.xkeysnail-config/t430s-default.py --quiet </dev/null &>/dev/null &
