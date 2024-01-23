d2481b071120630ba41e14c0cfd1d8bbb107399a
Mon Noval <msnoval@d-edge.com>
2024-01-23 16:36:38 +0800

    lots

---
+++ run-minipc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py
+# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event3 /dev/input/event6 --quiet </dev/null &>/dev/null &
+# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event3 /dev/input/event7 --quiet </dev/null &>/dev/null &
+# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event4 /dev/input/event7 --quiet </dev/null &>/dev/null &
+xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event4 /dev/input/event8 --quiet </dev/null &>/dev/null &
+# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event3 /dev/input/event9 --quiet </dev/null &>/dev/null &
--- run-pc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-xkeysnail $HOME/.xkeysnail-config/pc-kinesis-elecomexg.py --devices /dev/input/event3 /dev/input/event6 --quiet </dev/null &>/dev/null &
-# xkeysnail $HOME/.xkeysnail-config/pc-kinesis-elecomexg.py --devices /dev/input/event3 /dev/input/event8 --quiet </dev/null &>/dev/null &
-
-# xkeysnail $HOME/.xkeysnail-config/pc-elecomhuge.py --devices /dev/input/event9 --quiet </dev/null &>/dev/null &
