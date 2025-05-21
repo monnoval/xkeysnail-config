7a923178adceb46c6d7472a9972eab8cf8ba8e2a
Mon Noval <msnoval@d-edge.com>
2025-05-21 12:17:36 +0800

    cleanup

---
+++ run-nitro.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# xkeysnail ~/.xkeysnail-config/nitro5-kinesis-elecomhuge.py --devices /dev/input/event5 /dev/input/event8
+xkeysnail ~/.xkeysnail-config/nitro5-kinesis-elecomhuge.py --devices /dev/input/event5 /dev/input/event8 --quiet </dev/null &>/dev/null &
