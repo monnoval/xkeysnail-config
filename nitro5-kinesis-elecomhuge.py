bb920db5f6e281b9cd61fcacfdd4f2ad205e385c
Mon Noval <msnoval@d-edge.com>
2025-05-21 12:13:36 +0800

    minimal use of xkeysnail

--- nitro5-kinesis-elecomhuge.py
+++ nitro5-kinesis-elecomhuge.py
@@ -5,51 +5,21 @@ from xkeysnail.transform import *
 
 # [Global modemap] Change modifier keys as in xmodmap
 define_modmap({
-    # Kinesis Advantage
-    Key.RIGHT_META: Key.RIGHT_CTRL,
-    Key.LEFT_CTRL: Key.RIGHT_META,
-
     # ELECOM HUGE
     Key.BTN_BACK: Key.ENTER,
     Key.BTN_SIDE: Key.ENTER,
     Key.BTN_EXTRA: Key.BTN_MOUSE,
 })
 
-# [Conditional modmap] Change modifier keys in certain applications
-define_conditional_modmap(re.compile('nvim-qt'), {
-    # Kinesis Advantage
-    Key.RIGHT_META: Key.RIGHT_CTRL,
-})
-
 # Keybindings for Browsers
-define_keymap(re.compile("nvim-qt"), {
-    # ELECOM HUGE
-    K("BTN_FORWARD"): K("C-q"),
-}, "Gvim")
-
-# Keybindings for Browsers
-define_keymap(re.compile("Firefox|firefox|Google-chrome|Chromium|Brave"), {
+define_keymap(re.compile("firefox|chromium|Brave"), {
     K("C-right"): K("C-TAB"),
     K("C-left"): K("C-Shift-TAB"),
 }, "Browsers")
 
 # Affects all apps
 define_keymap(lambda wm_class: wm_class not in ("AnAppThatDoesNotExist"), {
-
-    # Linux apps
-    K("RShift-V"): [launch(["nvim-qt"])],
-    K("RShift-X"): [launch(["xkill"])],
-    K("RShift-W"): [launch(["brave-browser-beta","about:newtab"])],
-
-    # Plasma launch apps
-    K("RShift-T"): [launch(["konsole","--profile","work"])],
-    K("RShift-F"): [launch(["dolphin"])],
-
     # ELECOM HUGE
     K("BTN_FORWARD"): K("C-w"),
-    K("BTN_TASK"): K("C-f7"),
-
-    # Kinesis Advantage
-    K("HOME"): K("C-f9"),
-
+    K("BTN_TASK"): K("LShift-HOME"),
 }, "All apps")
--- run-nitro5.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-xkeysnail ~/.xkeysnail-config/nitro5-kinesis-elecomhuge.py --devices /dev/input/event8 /dev/input/event14 --quiet </dev/null &>/dev/null &
